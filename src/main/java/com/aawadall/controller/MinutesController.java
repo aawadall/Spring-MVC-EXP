640540fd3e27945c8a9d49f999f3068bc38788f2
aawadall <aawadall@ualberta.ca>
2019-07-18 20:23:41 -0400

    added forwarding

--- src/main/java/com/aawadall/controller/MinutesController.java
+++ src/main/java/com/aawadall/controller/MinutesController.java
@@ -13,6 +13,13 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		
 		System.out.println("Exercise: "+exercise.getMinutes());
+		return "forward:addMoreMinutes.html";
+	}
+	
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		
+		System.out.println("Exercise More: "+exercise.getMinutes());
 		return "addMinutes";
 	}
 }
